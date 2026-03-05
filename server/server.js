bd2e8fd79c981eef0e30e51a3863b1a25176f93b
tkhanhsss <it.tungkhanh@gmail.com>
2026-03-06 01:43:08 +0700

    add user model

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+await connectDB();
+
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ["http://localhost:5173"];
 
 // Middleware configuration
 app.use(express.json());
@@ -16,5 +20,5 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => res.send("API is Working"));
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
